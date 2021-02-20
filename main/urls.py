ce19a7cf7d6d32cf8cfac75f0948742a7762b878
Aditya <aditya@Adityas-MacBook-Air.local>
2021-02-20 20:19:34 +0530

    added a way to update posts

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import Home , Detail , AddPost
+from . views import Home , Detail , AddPost , UpdatePost
 
 urlpatterns = [
     path('' , Home.as_view() , name='main'),
     path('article/<int:pk>' , Detail.as_view() , name='article'),
     path('create' , AddPost.as_view() , name = "create"),
+    path('update/article/<int:pk>' , UpdatePost.as_view() , name="update")
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DetailView , CreateView
+from django.views.generic import ListView , DetailView , CreateView , UpdateView
 from . models import Post
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 # Create your views here.
 #def main(request):
     #return render(request , 'main.html' , {})
@@ -17,3 +19,9 @@ class AddPost(CreateView):
     model = Post
     template_name = "addpost.html" 
     fields = "__all__"
+
+class UpdatePost(SuccessMessageMixin , UpdateView):
+    model = Post
+    template_name = "updatepost.html"
+    fields = ['title' , 'body' , 'author']
+
